173ed16225baa40ceab0694db472bf8fd1c5228e
The Waggle <artuoh76@gmail.com>
2024-07-14 23:22:04 +0200

    fix publish

--- Out_of_Office_API/Program.cs
+++ Out_of_Office_API/Program.cs
@@ -65,7 +65,7 @@ builder.Services.AddAzureClients(clientBuilder =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
